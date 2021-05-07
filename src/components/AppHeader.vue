97610540fda67f716e88d0c25af520b96dab113f
Everton Alcides Buzzi <evertonbuzzi@gmail.com>
2021-05-07 10:01:40 -0300

    EB: Unit testing component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div
+    button(v-show="loggedIn") Logout
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loggedIn: false
+    }
+  }
+}
+</script>
